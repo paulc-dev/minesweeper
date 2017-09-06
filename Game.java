a3455c4f851328eaa461939f8fc7ab1367f8c0d5
Paul Cummings <paulcummings01@gmail.com>
2017-09-05 18:57:21 -0700

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,14 @@
+package com.github.minesweeper;
+
+
+
+public class Game {
+    public static void main(String args[]) {
+        // display a welcome message
+
+        
+        GameWindow launch = new GameWindow();
+         
+    }
+
+}
